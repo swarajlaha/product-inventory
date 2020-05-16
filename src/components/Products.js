a571853c0615d043d4bd91f5d553408825b46da5
swarajlaha <swaraj.srj@gmail.com>
2020-05-16 21:09:02 +0530

    add edit btn for each product

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
+import UserStore from '../stores/UserStore'
+import Button from 'react-bootstrap/Button'
 
 export default class Product extends React.Component {
   constructor(props) {
@@ -13,8 +15,8 @@ export default class Product extends React.Component {
   handleChange(evt) {
     this.props.onCheck(this.props.productId, evt.target.checked)
   }
-
   render() {
+    const currentUser = UserStore.getCurrentUser()
     return (
             <tr>
                 {this.props.columns.productName ? <td>  
@@ -23,10 +25,22 @@ export default class Product extends React.Component {
                 {this.props.columns.manufacturer ?<td>{this.props.manufacturer}</td> : null}
                 {this.props.columns.price ?<td>{this.props.price}</td> : null} 
                 {this.props.columns.quantity ?<td>{this.props.quantity}</td> : null}
-                  {this.props.columns.checkToSelect ?<td><Form.Check 
+                {this.props.columns.checkToSelect ?<td><Form.Check 
                     aria-label="option 1" 
                     onChange={this.handleChange} />
                   </td> : null}
+                {currentUser ?
+                (this.props.columns.edit ? <td>
+                  <Button variant="outline-success" size="sm" href={`/editProduct/${this.props.productId}`}>
+                  Edit
+                    </Button>
+                  </td> : null) :
+                  (this.props.columns.edit ? <td>
+                    <Button variant="outline-success" size="sm" disabled>
+                      Edit
+                    </Button>
+                    </td> : null)
+                  }
             </tr>
     )
   }
